7ea76b7c7d1eb68827eb1850db6d890b65d491da
blessedjoe2000 <blessedjoe2000@yahoo.com>
2025-01-05 17:47:06 -0600

    domain changed

--- wp-content/themes/healthcareimpact/functions.php
+++ wp-content/themes/healthcareimpact/functions.php
@@ -47,8 +47,8 @@ add_action('after_switch_theme', 'healthcare_flush_rewrite_rules');
 function add_cors_http_header() {
     $allowed_origins = array(
         'http://localhost:3000',
-        'https://www.hcimag.org',
-        'https://hcimag.org'
+        'https://www.hcimpactmag.com',
+        'https://hcimpactmag.com'
     );
 
     if (isset($_SERVER['HTTP_ORIGIN']) && in_array($_SERVER['HTTP_ORIGIN'], $allowed_origins)) {
@@ -70,8 +70,8 @@ add_action('rest_api_init', 'add_cors_http_header');
 function allow_cors_for_images($headers) {
     $allowed_origins = array(
         'http://localhost:3000',
-        'https://www.hcimag.org',
-        'https://hcimag.org'
+        'https://www.hcimpactmag.com',
+        'https://hcimpactmag.com'
     );
 
     if (isset($_SERVER['HTTP_ORIGIN']) && in_array($_SERVER['HTTP_ORIGIN'], $allowed_origins)) {
